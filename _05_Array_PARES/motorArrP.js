4a11a352dce5eec4f46fa2dd62672190f843ea17
Diego <48282657+Mijayo@users.noreply.github.com>
2019-10-02 09:25:58 +0200

    Comm4

---
+++ _05_Array_PARES/motorArrP.js
@@ -0,0 +1,6 @@
+// Definimos el Array
+var arrPares = new Array(2, 4, 6, 8, 10, 12, 14, 16);
+console.log(arrPares);
+
+// Mostramos el Array
+document.getElementById("arrPares").innerHTML = ' ' + arrPares + ' ';
