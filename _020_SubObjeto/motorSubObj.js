fef55f183e01ad18be1e52d5f79d52fabb58bcef
Diego <48282657+Mijayo@users.noreply.github.com>
2019-10-02 13:53:21 +0200

    Comm12

--- _020_SubObjeto/motorSubObj.js
+++ _020_SubObjeto/motorSubObj.js
@@ -3,11 +3,11 @@ var coche = {
     marca: 'Ford',
     modelo: 'Fiesta',
     color: 'Negro',
-    acabado: [{
-        Premium: 'Climatizador',
-        Elite: 'Lunas tintadas',
-        GT: 'Terminacion racing'
-    }],
+    acabado: {
+        premium: 'Climatizador',
+        elite: 'Lunas tintadas',
+        gT: 'Terminacion racing'
+    },
     peso: [1500, 1800, 1500, 1340],
     contaminacion: 0,
     combustible: 'Diesel',
@@ -21,8 +21,8 @@ document.getElementById("objC").innerHTML = ' ' + coche.marca;
 document.getElementById("marca").innerHTML = ' ' + coche.marca;
 document.getElementById("modelo").innerHTML = ' ' + coche.modelo;
 document.getElementById("color").innerHTML = ' ' + coche.color;
-document.getElementById("acabado").innerHTML = ' ' + coche.acabado[0] + ' - ' + coche.acabado[1] +
-    ' - ' + coche.acabado[2] + ' - ' + coche.acabado[3];
+document.getElementById("acabado").innerHTML = ' ' + coche.acabado.premium + ' - ' + coche.acabado.elite +
+    ' - ' + coche.acabado.gT;
 document.getElementById("peso").innerHTML = ' ' + coche.peso[0] + ' - ' + coche.peso[1] +
     ' - ' + coche.peso[2] + ' - ' + coche.peso[3];
 document.getElementById("contaminacion").innerHTML = ' ' + coche.contaminacion;
