7ebbf72032f40ffe25ed8d46e61006131c643992
Diego <48282657+Mijayo@users.noreply.github.com>
2019-10-02 09:55:01 +0200

    Comm6

---
+++ _08_Array_Dias_Laborables/motorArrDL.js
@@ -0,0 +1,9 @@
+// Definimos el Array
+var arrDias = ['Lunes', 'Martes', 'Miercoles',
+    'Jueves', 'Viernes'
+];
+console.log(arrDias);
+
+
+// Mostramos el Array
+document.getElementById("arrDL").innerHTML = ' ' + arrDias;
