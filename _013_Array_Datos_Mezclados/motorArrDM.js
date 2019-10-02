35db4bf42c3abece3fae36e44f00370efca2d4ed
Diego <48282657+Mijayo@users.noreply.github.com>
2019-10-02 11:20:54 +0200

    Comm8

---
+++ _013_Array_Datos_Mezclados/motorArrDM.js
@@ -0,0 +1,8 @@
+// Definimos el Array
+var arrDM = ['Lunes', 5, 'Hola',
+    true, { pepe: 1 }
+];
+console.log(arrDM);
+
+// Mostramos el Array
+document.getElementById("arrTD").innerHTML = ' ' + arrDM;
---
+++ _014_Array_Pares_Impares/motorArrPI.js
@@ -0,0 +1,19 @@
+// Definimos el Array con datos Pares
+var arrDP = [2, 4, 6, 8, 10, 12];
+console.log(arrDP);
+
+// Definimos el Array con datos Impares
+var arrDI = [1, 3, 5, 7, 9, 12];
+console.log(arrDI);
+
+// Mostramos el Array datos Pares
+document.getElementById("arrDP").innerHTML = ' ' + arrDP;
+
+// Mostramos el tamaño del Array datos Pares
+document.getElementById("arrLDP").innerHTML = ' ' + arrDP.length;
+
+// Mostramos el Array datos Impares
+document.getElementById("arrDI").innerHTML = ' ' + arrDI;
+
+// Mostramos el tamaño del Array datos Impares
+document.getElementById("arrLDI").innerHTML = ' ' + arrDI.length;
