1074b9576f8516a220763d2d049f7832c796f6f5
Mijayo <48282657+Mijayo@users.noreply.github.com>
2019-10-04 09:54:06 +0200

    Comm17

--- _024_Try_Catch/motorTrCa.js
+++ _024_Try_Catch/motorTrCa.js
@@ -1,4 +1,37 @@
-var d = new Date("10/25/1989");
+// Def funcion numerica
+function misNumeros() {
+  var mens, x;
+  mens = prompt("Introduce un numero entre 5 y 10");
+  try {
+    if (mens == " ") {
+      document.getElementById("campV").innerHTML = " campo vacio ";
+      throw "campo vacio";
+    }
+    if (isNaN(mens)) {
+      document.getElementById("noN").innerHTML =
+        " el valor introducido no es un numero ";
+      throw "el valor introducido no es un numero";
+    }
+    // transformamos a numero
+    x = Number(mens);
+    if (x < 5) {
+      document.getElementById("numIn").innerHTML = " numero mas bajo que el 5 ";
+      throw "numero mas bajo que el 5";
+    }
+    if (x > 10) {
+      document.getElementById("numSup").innerHTML =
+        " numero mas grande que el 10 ";
+      throw "numero mas grande que el 10";
+    }
+    if (x >= 5 || x <= 10) {
+        document.getElementById("numCo").innerHTML = ' ' + x;
+    }
+  } catch (err) {
+    // message.innerHTML = "Input is " + err;
+  }
+}
+
+/*var d = new Date("10/25/1989");
 
 if (Object.prototype.toString.call(d) === "[object Date]") {
     // it is a date
@@ -12,6 +45,6 @@ if (Object.prototype.toString.call(d) === "[object Date]") {
     // not a date
 }
 
-/*console.log(d.toString()); shows 'Invalid Date'
+console.log(d.toString()); shows 'Invalid Date'
 console.log(typeof d);  shows 'object'
 console.log(d instanceof Date);  shows 'true'*/
